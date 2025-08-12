37a6e2a5be3776045d736e76f13a815b872dd9ed
Grazielle Gualter <grazi.gualter@gmail.com>
2025-08-12 10:39:40 -0300

    01_Configurando_Projeto

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+
+
+@Module({
+  imports: [TypeOrmModule.forRoot ({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '212055',
+    database: 'db_projeto_final_bloco_02',
+    entities: [],
+    synchronize: true,
+    logging: true,
+
+})],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/categoria/controllers/categoria.controller.ts
@@ -0,0 +1,1 @@
+//criação categoria controller
